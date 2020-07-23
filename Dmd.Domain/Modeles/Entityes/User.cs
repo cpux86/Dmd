e4f4e26a58f3cd7d518431e13ce29380a6b218e3
cpux86 <cpux86@mail.ru>
2020-07-23 23:47:03 +0300

    Использование в БД GUID и Primary Key

--- Dmd.Domain/Modeles/Entityes/User.cs
+++ Dmd.Domain/Modeles/Entityes/User.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace Dmd.Domain.Modeles.Entityes
 {
     public class User
     {
+        public User()
+        {
+            this.Guid = Guid.NewGuid();
+        }
+
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //public Guid Identity { get; set; }
         public int Id { get; set; }
+
+        public Guid Guid { get; set; }
         public string FirstName { get; set; }
         /// <summary>
         /// Фамилия
